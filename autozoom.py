7659b858c71f768518eef68446af9d08cb7bb8ad
Jonathan Newnham <jnnnnn@gmail.com>
2021-06-30 15:57:40 +1000

    handle disconnects

--- autozoom.py
+++ autozoom.py
@@ -2,20 +2,31 @@
 from datetime import datetime, timedelta, timezone
 import pickle
 import os.path
-import pprint
 import re
 import time
 import logging
 import webbrowser
 
-logging.basicConfig(format="%(asctime)s %(message)s", level="INFO")
-logger = logging.getLogger()
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
+try:
+    logging.basicConfig(format="%(asctime)s %(message)s", level="INFO")
+    logger = logging.getLogger()
+    from googleapiclient.discovery import build
+    from google_auth_oauthlib.flow import InstalledAppFlow
+    from google.auth.transport.requests import Request
+except:
+    print(
+        """Import dependencies failed. You need to run the following command:
+
+        pip install -r requirements.txt
+        
+        """
+    )
+    exit()
 
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
+
+CHECK_INTERVAL_MINUTES = 10
 
 
 def getcreds():
@@ -31,6 +42,13 @@ def getcreds():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
+            if not os.path.exists("credentials.json"):
+                print(
+                    """Visit this page and click "create credentials" to obtain a credentials.json file:
+                https://developers.google.com/calendar/quickstart/python
+                """
+                )
+                exit()
             creds = InstalledAppFlow.from_client_secrets_file(
                 "credentials.json", SCOPES
             ).run_local_server(port=0)
@@ -40,55 +58,62 @@ def getcreds():
     return creds
 
 
+def getNextEvent(events):
+    for event in events:
+        title = event.get("summary", "<no summary>")
+        logger.info(f"Inspecting event '{title}'")
+        zoomlink = getzoomlink(event)
+        if not zoomlink:
+            logger.info(f"zoomlink not found, skipping")
+            continue
+        waittime = timeuntilstart(event)
+        if waittime.total_seconds() < 0:
+            logger.info(f"event has already started, skipping")
+            continue
+        return zoomlink, waittime
+    else:
+        logger.info(f"Checked {len(events)} events!")
+        return "", timedelta(days=1)
+
+
 def main():
     service = build("calendar", "v3", credentials=getcreds())
 
     while True:
-        events = getevents(service)
-
-        for event in events:
-            title = event.get("summary", "<no summary>")
-            logger.info(f"Inspecting event '{title}'")
-            zoomlink = getzoomlink(event)
-            if not zoomlink:
-                logger.info(f"zoomlink not found, skipping")
-                continue
-            waittime = timeuntilstart(event)
-            if waittime.total_seconds() < 0:
-                logger.info(f"event has already started, skipping")
-                continue
-            break
-        else:
-            logger.info("Checked ten events!")
-
-        if waittime.total_seconds() < 3600:
+        zoomlink, waittime = getNextEvent(getevents(service))
+        if zoomlink and waittime.total_seconds() < CHECK_INTERVAL_MINUTES * 60:
             logger.info(f"zoom-linked event starting in {waittime}...")
             # connecting takes about seven seconds, so start connecting early
             time.sleep(waittime.total_seconds())
             logger.info(f"connecting now, link is {zoomlink}")
             webbrowser.open(zoomlink)
         else:
-            logger.info(f"event is {waittime} away, will check again in an hour")
-            time.sleep(3600)
+            logger.info(
+                f"event is {waittime} away, will check again in {CHECK_INTERVAL_MINUTES} minutes."
+            )
+            time.sleep(CHECK_INTERVAL_MINUTES * 60)
 
 
 def getevents(service, N=10):
-
     # Call the Calendar API
     now = datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
     print("Getting the upcoming 10 events")
-    events_result = (
-        service.events()
-        .list(
-            calendarId="primary",
-            timeMin=now,
-            maxResults=N,
-            singleEvents=True,
-            orderBy="startTime",
+    try:
+        events_result = (
+            service.events()
+            .list(
+                calendarId="primary",
+                timeMin=now,
+                maxResults=N,
+                singleEvents=True,
+                orderBy="startTime",
+            )
+            .execute()
         )
-        .execute()
-    )
-    events = events_result.get("items", [])
+        events = events_result.get("items", [])
+    except Exception as e:
+        print(e)
+        return []
 
     print(f"Retrieved {len(events)} events.")
     return events
